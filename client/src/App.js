69e6d2a240a3c3b21220070464c03317b013cc4b
karlajrdz <karlajrdz@gmail.com>
2022-08-02 22:12:45 +0200

    Login cahnges

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import {Route , BrowserRouter , Redirect} from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import BookingCar from './pages/BookingCar'
+import BookingBike from './pages/BookingBike'
 import 'antd/dist/antd.css';
 import UserBookings from './pages/UserBookings';
-import AddCar from './pages/AddCar';
+import AddBike from './pages/AddBike';
 import AdminHome from './pages/AdminHome';
-import EditCar from './pages/EditCar';
+import EditBike from './pages/EditBike';
 
 function App() {
   return (
@@ -22,16 +22,19 @@ function App() {
              <ProtectedRoute path='/' exact component={Home} />
              <Route path='/login' exact component={Login} />
              <Route path='/register' exact component={Register} />
-             <ProtectedRoute path='/booking/:carid' exact component={BookingCar} />
+             <ProtectedRoute path='/booking/:bikeid' exact component={BookingBike} />
              <ProtectedRoute path='/userbookings' exact component={UserBookings} />
-             <ProtectedRoute path='/addcar' exact component={AddCar} />
-             <ProtectedRoute path='/editcar/:carid' exact component={EditCar} />
+             <ProtectedRoute path='/addbike' exact component={AddBike} />
+             <ProtectedRoute path='/editbike/:bikeid' exact component={EditBike} />
              <ProtectedRoute path='/admin' exact component={AdminHome} />
          
          </BrowserRouter>
 
     </div>
   );
+  <div className=''>
+
+  </div>
 }
 
 
