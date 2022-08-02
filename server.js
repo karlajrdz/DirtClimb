a86bec32b8b966d13a84e76ed44042e486c71bd7
karlajrdz <karlajrdz@gmail.com>
2022-08-02 22:16:57 +0200

    register changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5002
+const port = process.env.PORT || 5000
 const dbConnection = require('./db')
 app.use(express.json())
 
