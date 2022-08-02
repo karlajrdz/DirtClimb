6a61c09940734e3fc069c591e0ef60f207300c32
karlajrdz <karlajrdz@gmail.com>
2022-08-02 22:39:36 +0200

    Adding bikes database

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,12 @@ import {Route , BrowserRouter , Redirect} from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import BookingBike from './pages/BookingBike'
+import BookingCar from './pages/BookingCar'
 import 'antd/dist/antd.css';
 import UserBookings from './pages/UserBookings';
-import AddBike from './pages/AddBike';
+import AddCar from './pages/AddCar';
 import AdminHome from './pages/AdminHome';
-import EditBike from './pages/EditBike';
+import EditCar from './pages/EditCar';
 
 function App() {
   return (
@@ -22,19 +22,16 @@ function App() {
              <ProtectedRoute path='/' exact component={Home} />
              <Route path='/login' exact component={Login} />
              <Route path='/register' exact component={Register} />
-             <ProtectedRoute path='/booking/:bikeid' exact component={BookingBike} />
+             <ProtectedRoute path='/booking/:carid' exact component={BookingCar} />
              <ProtectedRoute path='/userbookings' exact component={UserBookings} />
-             <ProtectedRoute path='/addbike' exact component={AddBike} />
-             <ProtectedRoute path='/editbike/:bikeid' exact component={EditBike} />
+             <ProtectedRoute path='/addcar' exact component={AddCar} />
+             <ProtectedRoute path='/editcar/:carid' exact component={EditCar} />
              <ProtectedRoute path='/admin' exact component={AdminHome} />
          
          </BrowserRouter>
 
     </div>
   );
-  <div className=''>
-
-  </div>
 }
 
 
